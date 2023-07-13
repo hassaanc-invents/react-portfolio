63a6392a08ba1d935fad7504e5ef5616d4c50ad8
Hassaan Malik <hassaantechspot@gmail.com>
2023-07-13 14:20:14 +0500

    NAvbar Setting

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import AppRouter from './Router';
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <AppRouter/>
     </div>
   );
--- src/views/_/navigation/Index.js
+++ src/views/_/navigation/Index.js
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+} from "reactstrap";
 import { Link } from "react-router-dom";
+import NavbarIcon from "../../../assets/icons/burger-icon.svg";
 import NavigationConfig from "../../../config/NavigationConfig";
 
 const Navigation = () => {
+  const [collapsed, setCollapsed] = useState(true);
+  const toggleNavbar = () => setCollapsed(!collapsed);
+
   return (
-    <nav>
-      <ul>
-        {Array.isArray(NavigationConfig) &&
-          NavigationConfig.map((item) => (
-            <li key={item.path}>
-              <Link to={item.path}>{item.title}</Link>
-            </li>
-          ))}
-      </ul>
-    </nav>
+    <div>
+      <Navbar color="faded" dark expand="md">
+        <NavbarBrand href="/" className="me-auto">
+          &lt; <span id="brand">Hassaan </span>/&gt;
+        </NavbarBrand>
+        <NavbarToggler onClick={toggleNavbar} className="me-2">
+          <img src={NavbarIcon}></img>
+        </NavbarToggler>
+        <Collapse isOpen={!collapsed} navbar>
+          <Nav className="ms-auto" navbar>
+            {Array.isArray(NavigationConfig) &&
+              NavigationConfig.map((item) => (
+                <NavItem>
+                  <Link to={item.path} className="nav-link">
+                    {item.title}
+                  </Link>
+                </NavItem>
+              ))}
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
   );
 };
 
