f553b303ffcd7cb4abab6b330974067627fbbcf9
Hassaan Malik <hassaantechspot@gmail.com>
2023-07-13 12:14:46 +0500

    Bootstrap Setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import AppRouter from './Router';
-
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="App">
