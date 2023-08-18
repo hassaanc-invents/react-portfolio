505b3a34d27c24ec51838d254b50140151257b96
Asad Bhutta <malikasadali513@gmail.com>
2023-08-18 23:57:50 +0500

    Comment Added

--- src/views/pages/home/Index.jsx
+++ src/views/pages/home/Index.jsx
@@ -12,59 +12,65 @@ const Index = () => {
           class="grad-circle d-flex justify-content-center align-items-center "
           id="minicircle"
         >
-          <a href="https://github.com/hassaanc-invents/react-portfolio" target="_blank">
-            <GitHub color="white"/>
+          {/* Added By Hassaan */}
+          <a
+            href="https://github.com/hassaanc-invents/react-portfolio"
+            target="_blank"
+          >
+            <GitHub color="white" />
           </a>
         </div>
       </Row>
       <Row>
         <Col lg="6" className="pt-5">
-          <Button className="experttitle px-4 mt-5">Full Stack Web Expert</Button>
+          <Button className="experttitle px-4 mt-5">
+            Full Stack Web Expert
+          </Button>
           <h1 className="first-name mt-3">Muhammad</h1>
           <h1 className="last-name">Hassaan</h1>
           <div class="w-100 social-media d-flex align-items-center mt-4">
-            <hr className="social-row"/>
-            {Array.isArray(SocialConfig) && SocialConfig.map((item)=>(
-            <a className="social-icon-wrapper cursor-pointer rounded-circle d-flex align-items-center justify-content-center ms-4" title={item.title} href={item.referTo} target="_blank">
-            {item.icon}
-            </a>
-            ))}
+            <hr className="social-row" />
+            {Array.isArray(SocialConfig) &&
+              SocialConfig.map((item) => (
+                <a
+                  className="social-icon-wrapper cursor-pointer rounded-circle d-flex align-items-center justify-content-center ms-4"
+                  title={item.title}
+                  href={item.referTo}
+                  target="_blank"
+                >
+                  {item.icon}
+                </a>
+              ))}
           </div>
           <div className="mt-5">
-          <a href="contact" className="text-uppercase chat-button">let's chat!</a>
+            <a href="contact" className="text-uppercase chat-button">
+              let's chat!
+            </a>
           </div>
           <Row className="mt-5">
             <Col lg="4" md="6" className="d-flex align-items-center mt-3">
-              <h3>
-                2+
-              </h3>
-              <span className="ms-2 home-lables">
-                Years Experience
-              </span>
+              <h3>2+</h3>
+              <span className="ms-2 home-lables">Years Experience</span>
             </Col>
             <Col lg="4" md="6" className="d-flex align-items-center mt-3">
-              <h3>
-                15+
-              </h3>
+              <h3>15+</h3>
               <span className="ms-2 home-lables">
                 Projects Completed in 10+ Countries
               </span>
             </Col>
             <Col lg="4" md="6" className="d-flex align-items-center mt-3">
-              <h3>
-                55k+
-              </h3>
-              <span className="ms-2 home-lables">
-                Content Reach & Views
-              </span>
+              <h3>55k+</h3>
+              <span className="ms-2 home-lables">Content Reach & Views</span>
             </Col>
           </Row>
         </Col>
         <Col lg="6" className="d-none d-lg-block">
-          <div className="w-100 h-auto dev-circle d-flex justify-content-between rounded-circle">
-          </div>
+          <div className="w-100 h-auto dev-circle d-flex justify-content-between rounded-circle"></div>
           <div className="d-flex justify-content-center align-items-center">
-          <img src={DeveloperImage} className="img-fluid main-img-fluid"></img>
+            <img
+              src={DeveloperImage}
+              className="img-fluid main-img-fluid"
+            ></img>
           </div>
         </Col>
       </Row>
